1d7a3663844c7049816cc07a96f53d386169a691
Ramana Rao <nandigamramanarao1@gmail.com>
2020-02-08 22:50:55 +0530

    Template driven and Reactive form example code changes

--- AngularApp/ClientApp/src/app/app.component.ts
+++ AngularApp/ClientApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { AccountService } from './accounts.service';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
   providers: [AccountService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor() {
     //console.log('c call');
@@ -73,15 +73,77 @@ export class AppComponent {
   //}
 
 
-    // Forms Code
+    // Forms Code template driven opproch
 
 
     //onSubmit(form: NgForm) {
     //    console.log(form);
     //}
 
+
+
     @ViewChild('f') signUpForm: NgForm
-    onSubmit() {
-        console.log(this.signUpForm);
+    defQuestion = 'pet'
+    genders = ['male', 'female']
+    user = {
+        uname: '',
+        email: '',
+        secreat: '',
+        answer: '',
+        gender:'',
+
     }
+    formSubmitted = false;
+
+    //using ngModelGroup on ui
+    sugUsername() {
+        const sUsername = 'user';
+        this.formSubmitted = false;
+        this, this.signUpForm.form.patchValue({
+            userData: {
+                userName: sUsername
+            }
+        });
+    }
+
+    onSubmit() {
+        //console.log(this.signUpForm);
+        this.formSubmitted = true;
+
+        this.user.uname = this.signUpForm.value.userData.userName;
+        this.user.email = this.signUpForm.value.userData.email;
+        this.user.secreat = this.signUpForm.value.secret;
+        this.user.gender = this.signUpForm.value.gender;
+        this.user.answer = this.signUpForm.value.answers;
+        this.signUpForm.reset();
+    }
+
+    //Reactive driven opproch
+
+    Genders = ['male', 'female'];
+
+    sForm: FormGroup;
+
+    ngOnInit() {
+        this.sForm = new FormGroup({
+            'userData': new FormGroup({
+                'username': new FormControl(null, Validators.required),
+                'email': new FormControl(null, [Validators.required, Validators.email])
+            }),            
+            'gender': new FormControl('male'),
+            'hobbies': new FormArray([])
+        });
+
+    }
+
+    onAddHobby() {
+        const control = new FormControl(null, Validators.required);
+        (<FormArray>this.sForm.get('hobbies')).push(control);
+    }
+
+
+    ronSubmit() {
+        console.log(this.sForm);
+    }
+
 }
